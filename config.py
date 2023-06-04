1b8fea22cd3c861ab6d2addc4ff1ec76af87f06b
AnastasiyaTikhonenko <115412818+AnastasiyaTikhonenko@users.noreply.github.com>
2023-06-04 17:57:01 +0300

    Update config.py
    keyes are deleted

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from PIL.EpsImagePlugin import binary
 
-comm_token = 'vk1.a.A8eWICX4HjhuasZzKks7rRxhACfq7bmftRZosnUoSr5dduTB6894a6AcY3yZkjuBPcaXG-shBGvN2Drgr9V5XlaXABuSZrKYTyDEADYICfF_azEBGY1lKMdic3j53pnHbPU3MzseBs-oGpY-W0-8Qh9Pku8XDQIvaNPE5ymxs-uGBMDnTZJrd8pIc8t4_fHoDHl6pSc8IouqJimnu2cGuQ'
-my_token = 'vk1.a.4IuOLhC1jNCKG6iCnbcm0RN7KAtmTVMDrJQQL_-uQXFAdEOQj7rcCm_19f-8X9vs4bdxZZgzSntbelfS8y9KzjFJi_NGVBmRqv69PIJ2EJCQ3I-cCzzTQSJTdX9JN32X7owf3T3tTeHwrq7RtR7a4yGlrSu8kcqFVJY2CyeDtyLfISqWjX-X53MaQNJj-MNp'
+comm_token = ''
+my_token = ''
 
 localhost = '127.0.0.1'
 user = 'postgres'
-password = 'Boltolog17'
+password = ''
 db_name = 'vk_candidates'
